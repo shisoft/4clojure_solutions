cf700c1b6eef5658cd067c1094fa90b547741c54
shisoft <shisoftgenius@hotmail.com>
2013-10-06 04:40:48 +0800

    5

---
+++ 5.clj
@@ -0,0 +1,6 @@
+;;;When operating on a list, the conj function will return a new list with one or more items "added" to the front.
+
+(= '(1 2 3 4) (conj '(2 3 4) 1))
+(= '(1 2 3 4) (conj '(3 4) 2 1)))
+
+;;;Failed attempt '(2 3 4 1) '(3 4 2 1)
